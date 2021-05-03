2be82128d10add0cf35bb523d706682204bca7d8
Raza Bhatti <R.Bhatti@ceswest.ca>
2021-05-03 13:05:17 -0600

    updates

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app=express();
 
 app.get('/', (req,res) => {
-    res.send('CES App Version 10.70');
+    res.send('CES App Version 10.70 on Desktop');
 });
 
 app.listen(8080, () => {
